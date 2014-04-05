9ca73162b9437d8a54ebd07b598db8b9021cae9a
karthikDDS <karthik10.shankar@gmail.com>
2014-04-05 11:17:45 -0400

    UDPserver.h
    Please include this file when myrecv() is to be used.

---
+++ UDPserver.h
@@ -0,0 +1,14 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <string.h>
+
+
+
+typedef enum {SUCCESS, FAILURE , RETRY} ErrorCode;
+
+void* myrecv(void);
