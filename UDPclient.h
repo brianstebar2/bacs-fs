a8b71db40ab9f7f329bd7a59cd1ef79cba744e8d
karthikDDS <karthik10.shankar@gmail.com>
2014-04-05 11:21:52 -0400

    UDPclient header.
    Please include this when mysend() needs to be called.

---
+++ UDPclient.h
@@ -0,0 +1,16 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <string.h>
+
+
+typedef enum {SUCCESS, FAILURE , RETRY} ErrorCode;
+
+
+
+ErrorCode mysend(void* p, char* IPaddr);
+
