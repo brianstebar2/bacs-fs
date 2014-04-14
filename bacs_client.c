003c89a8cb511f7cbcd69e31b1222cce0be4fbf4
cpatel60 <cpatel60@users.noreply.github.com>
2014-04-14 14:44:08 -0400

    Update bacs_client.c

--- bacs_client.c
+++ bacs_client.c
@@ -12,43 +12,193 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <UDPserver.h>
+#include "send_file.h"
 
-#define MAX_LENGTH 10
-#define FILE 0
-#define DIR 1
+void change_dir(char* dir, char* path)
+{
+	char dirpath[1024];
+	strcpy(dirpath,path);
+	strcat(dirpath,"/");
+	strcat(dirpath,dir);
+	printf("dirpath:%s\n",dirpath);
+	int result = chdir(dirpath);
+	if(result==0)
+	{
+		strcat(path,"/");
+		strcat(path,dir);
+		printf("Changed to directory '%s'\n", path);
+	}
+	else
+        	perror("mkdir() error");
+}
 
-upload(char* file_name, bool f, char* local_path, char* remote_path);
-download(char* file_name, bool f, char* local_path, char* remote_path);
-change_local_dir(char* path, char* local_path);
-change_remote_dir(char* path, char* remote_path);
+void list_dir(char* path)
+{
+	DIR *dp;
+  	struct dirent *ep;     
+  	dp = opendir (path);
+	if (dp != NULL)
+	{
+		while (ep = readdir (dp))
+			puts (ep->d_name);
+		closedir (dp);
+	}
+  	else
+    		perror ("ls() error");
+}
+
+void make_dir(char* dir, bool l, char* path)
+{
+	if(l==0)
+	{
+		char dirpath[1024];
+		strcpy(dirpath,path);
+		strcat(dirpath,"/");
+		strcat(dirpath,dir);
+		printf("dirpath:%s\n",dirpath);
+		int result = mkdir(dirpath, 0777);
+		if(result==0)
+			printf("Local directory '%s' created at path: %s\n", dir, path);
+		else
+           		perror("mkdir() error");
+	}
+	else
+	{
+		printf("Remote directory '%s' created at path: %s\n", dir, path);
+	}
+}
+
 char* get_string(char *input, int index)
 {
 	int i=0;
 	int len = strlen(input);
-	char *string = malloc(sizeof(char)*MAX_LENGTH);
+	char *string = (char*)malloc(sizeof(char)*20);
+	memset(string,0,20);
 	int k, name_index=0;
 	while(index<len)
 	{
 		string[i++]=input[index++];
 	}
-	printf("%s",string);
+	//printf("%s",string);
 	return string;
+}
+
+void upload(char* file_name, bool f, char* local_path, char* remote_path, char* IPaddr)
+{
+	if(f==0)
+	{
+		printf("...Uploading file '%s'...\n", file_name);
+		char filepath[1024];
+		strcpy(filepath,local_path);
+		strcat(filepath,"/");
+		strcat(filepath,file_name);
+		send_file(filepath, IPaddr);
+		printf("\n...File uploaded to path: %s\n",remote_path);
+	}
+	else
+	{
+		char local_temp[1024], remote_temp[1024];
+		strcpy(local_temp, local_path);
+		strcpy(remote_temp, remote_path);
+		printf("Uploading directory '%s'\n", file_name);
+		make_dir(file_name, 1, remote_path);
+		change_dir(file_name, remote_path);
+		change_dir(file_name, local_path);
+
+		//upload individual files
+		DIR *dirp = opendir(local_path);;
+  		struct dirent *dp; 
+		while (dirp) 
+		{
+    			if ((dp = readdir(dirp)) != NULL) 
+			{
+				char filepath[1024];
+				strcpy(filepath,local_path);
+				strcat(filepath,"/");
+				strcat(filepath,dp->d_name);
+				send_file(filepath, IPaddr);
+				printf("\n...File uploaded to path: %s\n",remote_path);
+			}
+			closedir(dirp);
+		}
+        		
+		strcpy(local_path, local_temp);
+		strcpy(remote_path, remote_temp);
+		printf("Directory uploaded to path: %s\n",remote_path);
+	}
+}
+
+void download(char* file_name, bool f, char* local_path, char* remote_path)
+{
+	if(!f)
+	{
+		printf("Downloading file '%s'..\n", file_name);
+		void* ans;
+		while((ans = myrecv())!=1 ||(ans = myrecv())!=2)
+		{
+			fputs(ans, stdout);
+			printf("\n");
+		}
+
+		char filepath[1024];
+		strcpy(filepath,local_path);
+		strcat(filepath,"/");
+		strcat(filepath,file_name);
+		FILE *fp = fopen(filepath,"w");
+		fwrite(ans, 1, sizeof(ans), fp);
+   		fclose(fp);
+		printf("File downloaded to path: %s\n",local_path);
+	}
+	else
+	{		
+		printf("Downloading directory '%s'..\n", file_name);
+		char local_temp[1024], remote_temp[1024];
+		strcpy(local_temp, local_path);
+		strcpy(remote_temp, remote_path);
+		make_dir(file_name, 0, local_path);
+		change_dir(file_name, local_path);
+		change_dir(file_name, remote_path);
+
+		//download all files in dir
+
+		strcpy(local_path, local_temp);
+		strcpy(remote_path, remote_temp);
+		printf("Directory downloaded to path: %s\n",local_path);
+	}
 }
 
 int main(int argc, char **argv)
 {
-	//FILE *fp;
+	int MAX_LENGTH=20;
+	FILE *fp;
 	int i, index, j;
+	char IPaddr[1024];
 	char *command = (char *) malloc(sizeof(char)*MAX_LENGTH);
   	char *input = (char *) malloc(sizeof(char)*MAX_LENGTH);
-	char *local_path = (char *) malloc(sizeof(char)*MAX_LENGTH);
-	char *remote_path = (char *) malloc(sizeof(char)*MAX_LENGTH);
+	char *local_path = (char *) malloc(1024);
+	char *remote_path = (char *) malloc(1024);
+	memset(local_path,0, MAX_LENGTH);
+	memset(remote_path,0, MAX_LENGTH);
+	//strcpy(local_path,"/local");
+	strcpy(remote_path,"/remote");
+	strcpy(IPaddr, "127.0.0.1");
+	if (getcwd(local_path, 1024) != NULL)
+           	printf("\nlocal_path: %s\n", local_path);
+       	else
+           	perror("getcwd() error");
+	printf("remote_path: %s\n", remote_path);
+
 	while(1)
 	{
 		memset(input,0, MAX_LENGTH);
 		memset(command,0, MAX_LENGTH);
 		index=0;
-		printf("%s","\n>>");
+		printf("\n>>");
 		scanf(" %[^\n]s",input);
 	//	printf("%s %s","\ninput= ",input);
 		for(i=0; i<MAX_LENGTH; i++)
@@ -60,7 +210,7 @@ int main(int argc, char **argv)
 	//	printf("%s %s","\ncommand= ",command);
 		if(strcmp(command,"help")==0)
 		{
-		/*	fp=fopen("/home/charmi/Documents/HELP","r");
+			fp=fopen("/home/charmi/Documents/BACS/HELP","r");
 			fseek(fp, 0, SEEK_END);
 			long fsize = ftell(fp);
 			fseek(fp, 0, SEEK_SET);
@@ -68,7 +218,7 @@ int main(int argc, char **argv)
 			fread(string, fsize, 1, fp);
 			fclose(fp);
 			string[fsize] = 0;
-			printf("%s",string);*/
+			printf("%s",string);
 		}
 		else if(strcmp(command,"upload")==0)
 		{
@@ -78,12 +228,12 @@ int main(int argc, char **argv)
 				if(input[j+1]=='f')
 				{
 					char* string = get_string(input, j+3);
-					//upload(string, FILE, local_path, remote_path);
+					upload(string, 0, local_path, remote_path, IPaddr);
 				}
 				else if(input[j+1]=='d')
 				{
 					char* string = get_string(input, j+3);
-					//upload(string, DIR, local_path, remote_path);	
+					upload(string, 1, local_path, remote_path, IPaddr);	
 				}
 				else
 				{
@@ -103,12 +253,37 @@ int main(int argc, char **argv)
 				if(input[j+1]=='f')
 				{
 					char* string = get_string(input, j+3);
-					//download(string, FILE, local_path, remote_path);
+					download(string, 0, local_path, remote_path);
 				}
 				else if(input[j+1]=='d')
 				{
 					char* string = get_string(input, j+3);
-					//download(string, DIR, local_path, remote_path);
+					download(string, 1, local_path, remote_path);
+				}
+				else
+				{
+					printf("%s", "Syntax error.");
+				}
+			}
+			else
+			{
+				printf("%s", "Syntax error.");
+			}
+		}
+		else if(strcmp(command,"mkdir")==0)
+		{
+			j=index+1;
+			if(input[j]=='-' && input[j+2]==' ')
+			{
+				if(input[j+1]=='l')
+				{
+					char* string = get_string(input, j+3);
+					make_dir(string, 0, local_path);
+				}
+				else if(input[j+1]=='r')
+				{
+					char* string = get_string(input, j+3);
+					make_dir(string, 1, remote_path);
 				}
 				else
 				{
@@ -128,12 +303,12 @@ int main(int argc, char **argv)
 				if(input[j+1]=='l')
 				{
 					char* string = get_string(input, j+3);
-				//	change_local_dir(string, local_path);
+					change_dir(string, local_path);
 				}
 				else if(input[j+1]=='r')
 				{
 					char* string = get_string(input, j+3);
-				//	change_remote_dir(string, local_path);
+					change_dir(string, remote_path);
 				}
 				else
 				{
@@ -147,6 +322,28 @@ int main(int argc, char **argv)
 		}
 		else if(strcmp(command,"ls")==0)
 		{
+			j=index+1;
+			if(input[j]=='-')
+			{
+				if(input[j+1]=='l')
+				{
+					char* string = get_string(input, j+3);
+					list_dir(local_path);
+				}
+				else if(input[j+1]=='r')
+				{
+					char* string = get_string(input, j+3);
+					list_dir(remote_path);
+				}
+				else
+				{
+					printf("%s", "Syntax error.");
+				}
+			}
+			else
+			{
+				printf("%s", "Syntax error.");
+			}
 		}
 		else if(strcmp(command,"exit")==0)
 		{
