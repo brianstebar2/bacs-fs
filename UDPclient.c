0364bc51c510941210c16f46c563adc2e5763e30
skullers <sriram.bala227@gmail.com>
2014-04-22 18:25:29 -0400

    Update UDPclient.c

--- UDPclient.c
+++ UDPclient.c
@@ -1,4 +1,4 @@
-/*UDPClient.c*/
+//UDPClient.c
 
 
 #include <arpa/inet.h>
@@ -7,90 +7,103 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
 #include "UDPclient.h"
-
-#define BUFLEN 1024
-#define PORT 9930
+#include "math.h"
 
 
-void err(char *s)
+
+ErrorCode mysend(void* p, char* IPaddr, int PN, int size_of_blocks  )
 {
-	perror(s);
-	exit(1);
-}
 
-ErrorCode mysend(void* p, char* IPaddr )
-{	
+	struct Send_message send_message;
 
-	
+	int total_blocks = ceil(size_of_blocks/BUFLEN);
+
 	struct sockaddr_in serv_addr;
 	int sockfd, i, slen=sizeof(serv_addr);
-	char buf[BUFLEN];
+	
 	char rcvbuf[BUFLEN];
-	void* voidbuf = malloc(BUFLEN);
-	struct timeval timeout;
+	
+	char* rp = p;
+	char tot[3];
+	
 
-	/* Timer variables */
+	//Timer variables
+	struct timeval timeout;
 	timeout.tv_sec = 5;
-	timeout.tv_usec = 0; 
+	timeout.tv_usec = 0;
 
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
 	{
-		/*err("socket");*/
+	
 		printf("Error: Socket Not Created\n");
 		return FAILURE;
 	}
-		
+
 	bzero(&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
-	
+
 
 	if (inet_aton(IPaddr, &serv_addr.sin_addr)==0)
 	{
 		return FAILURE;
 	}
 
-	/*while(1)*/
+	
+	
+	
+	/*The first packet contains the number of blocks as the message */
+			
+	
+
+	for(i = 0; i <= total_blocks; i++)
 	{
-		/*printf("Clear the rcvbuf buffer\n");*/
 		memset(rcvbuf, 0, BUFLEN);
-		memcpy(buf, p, BUFLEN);			
-		if(strcmp(buf,"exit") == 0)
+		send_message.seq_number = i;
+		
+		if(i==0)
 		{
-			return FAILURE;
+			sprintf(send_message.message, "%d", total_blocks);
+			printf("Number of packets to be sent %s\n", send_message.message);
+		}
+		else
+		{
+			memcpy(send_message.message, rp, MESSAGE_LEN);
+			rp += MESSAGE_LEN;
 		}
 
-		printf("Sending Data\n");
-		if (sendto(sockfd,buf, BUFLEN, 0, (struct sockaddr*)&serv_addr, slen)==-1)		/*replacing buf with voidbuf*/
+		if (sendto(sockfd,	&send_message, sizeof(send_message), 0, (struct sockaddr*)&serv_addr, slen)==-1) //replacing buf with voidbuf
 		{
-			/*err("sendto()");*/
 			printf("sendto: Error\n\n");
 			return FAILURE;
 		}
-
-		/* Configure a recieve timer */
+		else
+			printf("sent: %s\n", send_message.message);
+		
+		//Configure a recieve timer
 		if(setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout,sizeof(timeout)) < 0)
 		{
 			printf("setsockopt: Error\n\n");
-			
 		}
-		/* Receive ACK from Server */
-		if (recvfrom(sockfd, rcvbuf, BUFLEN, 0, (struct sockaddr*)&serv_addr, (socklen_t*)&slen)==-1)
-		{	    	
+		//Receive ACK from Server
+		if (recvfrom(sockfd, rcvbuf, BUFLEN, 0, (struct sockaddr*)&serv_addr, &slen)==-1)
+		{    
 			return RETRY;
 		}
 		else
 		{
 			printf("Received response from server %s:%d:\t:%s\n\n",inet_ntoa(serv_addr.sin_addr),ntohs(serv_addr.sin_port), rcvbuf);
-			return SUCCESS;
+			//return SUCCESS;
 		}
 	}
+	
+	
+	
 
 	close(sockfd);
-	free(voidbuf);
-	
+	//free (rp);
 }
