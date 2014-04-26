e15485cdd21027cc49d3e0d5912766fdcf1b4a1d
cpatel60 <cpatel60@users.noreply.github.com>
2014-04-26 16:40:23 -0400

    Update send_file.c

--- send_file.c
+++ send_file.c
@@ -9,18 +9,19 @@ void send_file(char *path, char *IPaddr, uuid_t *uuids, uint64_t num_uuids)
 {
 	FILE *fp = fopen(path,"r");
 	char block[DEFAULT_BLOCK_SIZE];
-	size_t already_read_block;
 	int i=0;
-	fseek(fp,0,SEEK_SET);
 	int PN = 9930;
+	fseek(fp,0,SEEK_SET);
 	while(!feof(fp))
 	{
 		char *msg = 0;
 		uint64_t msg_len;
 		uuid_t uuid;
-		uint32_t size;
+		/*uint32_t size;*/
 		char *content = 0;
 		char *resp_msg = 0;
+		ErrorCode error;
+		struct Node* resp;
 		if(i>=num_uuids)
 			printf("****************error num_uuids");
 		/* Set block initially to 0 */
@@ -31,18 +32,17 @@ void send_file(char *path, char *IPaddr, uuid_t *uuids, uint64_t num_uuids)
 			printf("\n ERROR - EMPTY FILE!");
 				/* return EOF; */
 			}
-		already_read_block = fread(block, 1, DEFAULT_BLOCK_SIZE, fp);
+		fread(block, 1, DEFAULT_BLOCK_SIZE, fp);
 		create_msg_post_block_request(uuids[i], DEFAULT_BLOCK_SIZE, block, &msg, &msg_len);
 		/* mysend(msg...) */
-		ErrorCode error = mysend(block, IPaddr, PN, DEFAULT_BLOCK_SIZE);
+		error = mysend(block, IPaddr, PN, DEFAULT_BLOCK_SIZE);
 		if(error == FAILURE || error == RETRY)
 			printf("error in send");
 		
 		/* Wait for server response */
 		/* myrecv(..., resp_msg); */
-		struct Receive_message resp;
 		resp = myrecv();
-		resp_msg = resp.msg;
+		resp_msg = resp->message;
 		parse_msg_post_block_response(resp_msg, &uuid);
 		if(get_header_resource(resp_msg) != BACS_FILE || 
      	   	   get_header_action(resp_msg) != POST ||
