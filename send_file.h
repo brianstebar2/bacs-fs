4553bf04bedf72b6249886cb28269cb8a3749f08
cpatel60 <cpatel60@users.noreply.github.com>
2014-04-26 23:31:03 -0400

    Update send_file.h

--- send_file.h
+++ send_file.h
@@ -9,7 +9,7 @@
 
 #define SIZE 1024
 
-void send_file(char *path, char *IPaddr, uuid_t *uuids, uint64_t num_uuids);
+void send_file(char *path, long IPaddr, uuid_t *uuids, uint64_t num_uuids);
 
 
 #endif /* SEND_FILE_H */
